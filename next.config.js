61da6e8d4e37c4ce296ce5e6ef6c280d3cf6eefd
Andrei <andrei@palchys.com>
2021-12-24 13:34:38 +0300

    add rewrites and redirects

--- next.config.js
+++ next.config.js
@@ -1,4 +1,20 @@
 module.exports = {
+  async redirects() {
+    return [
+      {
+        source: '/redirect',
+        destination: '/ssr',
+      },
+    ]
+  },
+  async rewrite() {
+    return [
+      {
+        source: '/rewrite',
+        destination: '/ssr',
+      },
+    ]
+  },
   env: {
     SSR_TITLE: process.env.TITLE || 'Next.js',
   },
